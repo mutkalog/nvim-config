ea7c277961d622e77384586d396443c243f06a1b
Alexander Konyshev <akonyshev@mwnts.ru>
2026-05-13 17:14:38 +0300

    Switch to modern neovmim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,15 +2,26 @@ return {
     {
         "williamboman/mason.nvim",
         lazy = false,
-        opts = {
-            ensure_installed = { "clangd", "clang-format", "codelldb", "pyright",
-                "mypy", "ruff", "black", "cmake",
-                "bashls",
-            }
-        },
-        config = function(_, opts)
-            require("mason").setup(opts)
+        config = function()
+            require("mason").setup()
         end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        lazy = false,
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "clang-format",
+                "pyright",
+                "lua-language-server",
+                "cmake-language-server",
+                "bash-language-server",
+            },
+        },
     },
     {
         "williamboman/mason-lspconfig.nvim",
@@ -25,29 +36,36 @@ return {
         lazy = false,
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
+            vim.lsp.config("lua_ls", {
+                capabilities = capabilities,
             })
-            lspconfig.clangd.setup {
-                --capabilities = capabilities,
+
+            vim.lsp.config("clangd", {
                 cmd = { "clangd", "--background-index", "--clang-tidy" },
-                filetypes = { "cpp", "c" },
-            }
-            lspconfig.pyright.setup {
+                filetypes = { "c", "cpp" },
+            })
+
+            vim.lsp.config("pyright", {
                 capabilities = capabilities,
                 filetypes = { "python" },
-            }
-            lspconfig.cmake.setup {
+            })
+
+            vim.lsp.config("cmake", {
                 capabilities = capabilities,
                 filetypes = { "cmake" },
-            }
-            lspconfig.bashls.setup {
+            })
+
+            vim.lsp.config("bashls", {
                 capabilities = capabilities,
                 filetypes = { "sh" },
-            }
+            })
 
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("clangd")
+            vim.lsp.enable("pyright")
+            vim.lsp.enable("cmake")
+            vim.lsp.enable("bashls")
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "List information about the symbol under cursor" })
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to the definition" })
@@ -58,7 +76,7 @@ return {
             vim.keymap.set("v", "<leader>fm", vim.lsp.buf.format, { desc = "Format selection" })
             vim.keymap.set("n", "<leader>ic", vim.lsp.buf.incoming_calls, { desc = "List all the call sites of the symbol under cursor" })
             vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename, { desc = "Rename the symbol under cursor" })
-            vim.keymap.set("n",  "<leader>ch", ":ClangdSwitchSourceHeader<CR>", { desc = "Switch between source and header" })
+            vim.keymap.set("n", "<leader>ch", ":ClangdSwitchSourceHeader<CR>", { desc = "Switch between source and header" })
         end,
     }
 }
