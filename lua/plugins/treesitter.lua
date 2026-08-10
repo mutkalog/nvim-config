280e1530d7c5d1e8a3858d419c3ad6179a94c5d7
Alexander Konyshev <akonyshev@mwnts.ru>
2026-08-10 17:30:05 +0300

    Fix treesiter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,18 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-        ensure_installed = { "lua", "vim", "cpp", "vimdoc", "c" },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = false,
+        opts = {
+            ensure_installed = { "lua", "python", "matlab", "bash", "cmake", "vim", "cpp", "vimdoc", "c" },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
 
+        config = function(_, opts)
+            require('nvim-treesitter').install(opts.ensure_installed)
+            require('nvim-treesitter').setup(opts)
+        end,
     }
-  }
 }
