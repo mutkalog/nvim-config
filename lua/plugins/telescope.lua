b0d602a48d25405b6fbb2a571484fc6096411bf7
Alexander Konyshev <akonyshev@mwnts.ru>
2026-08-10 16:42:53 +0300

    Add rg with args plugin to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,6 @@ return {
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
     },
     {
-        "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
         "nvim-telescope/telescope.nvim",
         --tag = "0.1.5",
         dependencies = {
@@ -16,8 +13,8 @@ return {
         config = function()
             require("telescope").setup({
                 extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
+                    live_grep_args = {
+                        auto_quoting = false, -- enable/disable auto-quoting
                     },
                     fzf = {
                         fuzzy = true,                   -- false will only do exact matching
@@ -31,28 +28,16 @@ return {
             -- To get fzf loaded and working with telescope, you need to call
             -- load_extension, somewhere after setup function:
             require('telescope').load_extension('fzf')
+
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            -- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
-            --     additional_args = function()
-            --         return { "--hidden" }
-            --     end
-            -- })
-            vim.keymap.set("n", "<leader>fg", function()
-                builtin.live_grep({
-                    additional_args = function()
-                        return { "--hidden" }
-                    end
-                })
-            end)
+            vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
             vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})                   -- find buffers
             vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {}) -- find in current buffer
 
             vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})               -- git commits
             vim.keymap.set("n", "<leader>gs", builtin.git_status, {})                -- git status
-            -- vim.keymap.set("n", "<leader>fg", builtin.extensions.live_grep_args.live_grep_args, {})
-            require("telescope").load_extension("ui-select")
             require("telescope").load_extension("live_grep_args")
         end,
     },
